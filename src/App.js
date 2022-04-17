0d045916c21f34725f26fbb657b04004bef82c4f
itorro <nygtorrero98@gmail.com>
2022-04-16 23:12:07 -0600

    Implemented VideoListItem Component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import VideoListItem from "./components/VideoListItem";
 import youtube from "./api/youtube";
 
 class App extends React.Component {
+    state = { videoItems: [] };
     onSearchSubmit = async (search) => {
         const response = await youtube.get("/search", {
             params: { q: search },
         });
-
         console.log(response);
+        const videoItems = response.data.items.map((item) => {
+            return (
+                <VideoListItem key={item.id.videoId} videoItem={item.snippet} />
+            );
+        });
+        this.setState({ videoItems });
     };
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit} />
-                <VideoListItem />
-                <VideoListItem />
-                <VideoListItem />
+                {this.state.videoItems}
             </div>
         );
     }
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,7 +1,11 @@
 const VideoListItem = (props) => {
+    const { thumbnails, channelTitle, description, title } = props.videoItem;
     return (
         <div className="ui segment">
-            <p>Test</p>
+            <i src={thumbnails.default.url} alt="Video Thumbnail"></i>
+            <h1>{title}</h1>
+            <h3>{channelTitle}</h3>
+            <p>{description}</p>
         </div>
     );
 };
